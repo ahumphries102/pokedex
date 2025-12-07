7edf455ed129ab1fc7dd15e5d4c4edea7b204bc6
unknown <ahumphries@iprosystems.com>
2025-12-07 00:17:29 -0800

    Added stats to the screen

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,17 +1,54 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import "./pokedex.css"
 export default function ToDo() {
   const [pokeName, setPokeName] = useState("")
   const [pokemon, setPokemon] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [pokeStats, setPokeStats] = useState({})
   const handleInput = (e) => {
     setPokeName(e.target.value)
   }
   const locatePokemon = async (pokemon) => {
-    const response = await fetch("http://localhost:5000/" + pokemon)
-    const data = await response.json()
-    setPokemon(data)
-    setPokeName("")
+    setPokemon(null)
+    setError(null)
+    setLoading(true)
+    try {
+      const response = await fetch("http://localhost:5000/" + pokemon)
+      if (!response.ok) {
+        throw new Error("Pokemon not found!")
+      }
+      const data = await response.json()
+      setPokemon(data)
+      setPokeName("")
+      setPokeStats({
+        abilities: data.abilities.map(e => capitializeWord(e.ability.name)),
+        height: data.height,
+        type: data.types[0].type.name,
+        weight: data.weight,
+        stats: data.stats.map((e) => ({
+          name: capitializeWord(e.stat.name),
+          value: e.base_stat,
+        })),
+      })
+    } catch (error) {
+      setError(error)
+    } finally {
+      setLoading(false)
+    }
   }
+  const capitializeWord = (word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+  useEffect(() => {
+    locatePokemon("pikachu")
+    console.log("pokeStats updated:", pokeStats)
+    locatePokemon("pikachu")
+  }, [])
+
+  useEffect(() => {
+    console.log("pokeStats has been set:", pokeStats)
+  }, [pokeStats])
   return (
     <>
       <div className="pokedexContainer">
@@ -26,8 +63,16 @@ export default function ToDo() {
             <div className="topDots" />
             <div className="topDots" />
           </div>
-          <div className="screen">
-            {pokemon?.name}
+          <div
+            className="screen"
+            style={{ textAlign: "left", listStyle: "none", padding: 0 }}
+          >
+            <div>
+              {pokemon?.name}
+              </div>
+              <div>
+              Ability
+              </div>
             <div className="screen-pokemonImageContainer">
               <img
                 className="pokemonImage"
@@ -35,6 +80,37 @@ export default function ToDo() {
                 src={pokemon?.sprites?.other["official-artwork"].front_default}
                 alt={pokemon?.name}
               />
+              <ul style={{ textAlign: "left", listStyle: "none", padding: 0 }}>
+                <li>Height: {pokeStats.stats && pokeStats.height}ft</li>
+                <li>Weight: {pokeStats.stats && pokeStats.weight}lbs</li>
+              </ul>
+            </div>
+            {loading && <p>Loading...</p>}
+            {error && <p style={{ color: "red" }}>{error.message}</p>}
+            <div>
+              <ul style={{ textAlign: "left", listStyle: "none", padding: 0 }}>
+                {pokeStats.abilities &&
+                  pokeStats.abilities.map((ability, index) => (
+                    <li key={index}>{ability}</li>
+                  ))}
+              </ul>
+              <ul>
+                {pokeStats.forms &&
+                  pokeStats.forms.map((forms, index) => (
+                    <li key={index}>{forms.name}</li>
+                  ))}
+              </ul>
+            </div>
+            <div>
+              <p>Stats</p>
+              <ul style={{ textAlign: "left", listStyle: "none", padding: 0 }}>
+                {pokeStats.stats &&
+                  pokeStats.stats.map((stat, index) => (
+                    <li key={index}>
+                      {stat.name}: {stat.value}
+                    </li>
+                  ))}
+              </ul>
             </div>
           </div>
           <div className="screen-button"></div>
